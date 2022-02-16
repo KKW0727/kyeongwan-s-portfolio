e6965bbac34d631545e0eee9985dbf79d736e762
kkw0727 <kkw3401@gmaiil.com>
2022-02-16 18:01:22 +0900

    add: js Handle scrolling when tapping on the navbar menu

--- kyeongwan's-portfolio/index.js
+++ kyeongwan's-portfolio/index.js
@@ -26,3 +26,21 @@ navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
     navbarToggleBtn.classList.toggle('fa-times');
 });
+
+// Handle scrolling when tapping on the navbar menu
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+      return;
+    }
+    navbarTitle.classList.add('invisible');
+    navbarMenu.classList.remove('open');
+    navbarToggleBtn.classList.remove('fa-times');
+    scrollIntoView(link);
+  });
+  
+  function scrollIntoView(selector) {
+      const scrollTo = document.querySelector(selector);
+      scrollTo.scrollIntoView({ behavior: 'smooth'});
+    }
